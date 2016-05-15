4fc4e84004a06af6248f4360fc0784cf121427d6
Levon He <levonhe@163.com>
2016-05-15 17:59:48 +0800

    update

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -8,10 +8,40 @@ void swap(int& a,int& b){
 }
 void mergeSort(int array[],int n){
 }
+/*
+ *shellSort
+ */
 void shellSort(int array[],int n){
+}
+/*
+ *heapSort
+ */
+void heapAdjust(int array[],int s,int length){
+    int child=2*s+1;
+    int tmp=array[s];
+    while(child<length){
+        if(child+1<length && array[child]<array[child+1]){
+            child++;
+        }
+        if(array[s]<array[child]){
+            swap(array[s],array[child]);
+            s=child;
+            child=2*s+1;
+        }else{
+            break;
+        }
+    }
+}
+void buildHeap(int array[],int length){
+    for(int i=(length-1)/2;i>=0;i--){
+        heapAdjust(array,i,length);
+    }
 }
 void heapSort(int array[],int n){
 }
+/*
+ *insertSort
+ */
 void insertSort(int array[],int n){
     for(int i=1;i<n;i++){
         for(int j=i;j>0;j--){
@@ -61,6 +91,6 @@ int* creatRandomArray(int min,int max,int length){
 int main(){
     int* array=creatRandomArray(0,100,10);
     printArray(array,10);
-    selectionSort(array,10);
+    buildHeap(array,10);
     printArray(array,10);
 }
