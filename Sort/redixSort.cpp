8ed5ee0d7295e130a62d2e314095047e5dbc45ec
levon <levon.he@outlook.com>
2016-10-03 11:50:10 +0800

    update

--- Sort/redixSort.cpp
+++ Sort/redixSort.cpp
@@ -1,5 +1,32 @@
-void _redixSort(int a[],int n,int p,int k){
-
+#include <cstdlib>
+#include <math.h>
+#include <cstring>
+void _redixSort(int a[],int size,int p,int k){
+    int* temp=(int*)malloc(sizeof(int)*size);
+    int* count=(int*)malloc(sizeof(int)*k);
+    int pval=0;
+    int index=0;
+    for(int i=0;i<p;i++){
+        for(int j=0;j<k;j++){
+            count[j]=0;
+        }
+        pval=(int)pow((double)k,(double)i);
+        for(int j=0;j<size;j++){
+            index=int(a[j]/pval)%k;
+            count[index]=count[index]+1;
+        }
+        for(int j=1;j<k;j++){
+            count[j]=count[j]+count[j-1];
+        }
+        for(int j=size-1;j>=0;j--){
+            index=int(a[j]/pval)%k;
+            temp[count[index]-1]=a[j];
+            count[index]-=1;
+        }
+        memcpy(a,temp,size* sizeof(int));
+    }
+    free(count);
+    free(temp);
 }
 void redixSort(int a[],int n){
     _redixSort(a,n,3,10);
