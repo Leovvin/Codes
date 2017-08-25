f7154e9093dadddcac2e37eb0899d4f13ae4cc4e
hel39 <hel39@sany.com.cn>
2017-08-25 17:19:11 +0800

    add second kill app

--- SecondKill/mysql/install.sql
+++ SecondKill/mysql/install.sql
@@ -1,6 +1,6 @@
 create database if not exists SecondKill default character set utf8;
 use SecondKill;
-create table if not exists tmp(id smallint,val varchar(20));
-insert into tmp values (1,'jack'),(2,'robin'),(3,'mark');
-select * from tmp;
+create table if not exists user(id smallint,name varchar(20));
+insert into user values (1,'jack'),(2,'robin'),(3,'mark');
+select * from user;
 notee
