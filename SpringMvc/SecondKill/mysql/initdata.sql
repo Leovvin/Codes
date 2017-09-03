6e08f91de8f5468751fbd3a9e08059e66b3801a8
hel39 <hel39@sany.com.cn>
2017-09-03 17:25:43 +0800

    move SecondKill into SpringMvc

--- SpringMvc/SecondKill/mysql/initdata.sql
+++ SpringMvc/SecondKill/mysql/initdata.sql
@@ -1,4 +1,5 @@
 use SecondKill;
 insert into user values (1,'jack'),(2,'robin'),(3,'mark');
-select * from user;
+insert into goods values (1,'bike',100,100);
+select * from goods;
 notee
--- SpringMvc/SecondKill/mysql/install.sql
+++ SpringMvc/SecondKill/mysql/install.sql
@@ -2,3 +2,5 @@ create database if not exists SecondKill default character set utf8;
 use SecondKill;
 drop table if exists user;
 create table user(id bigint,name varchar(20));
+drop table if exists goods;
+create table goods(id bigint,name varchar(20),num int,price double);
