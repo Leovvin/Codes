2cc6d91853730290b31017dc405bfca2c5018996
hel39 <hel39@sany.com.cn>
2017-09-05 08:30:05 +0800

    update

--- SpringDemo/SecondKill/mysql/install.sql
+++ SpringDemo/SecondKill/mysql/install.sql
@@ -1,6 +1,10 @@
+-- table create
 create database if not exists SecondKill default character set utf8;
 use SecondKill;
-drop table if exists user;
-create table user(id bigint,name varchar(20));
 drop table if exists goods;
 create table goods(id bigint,name varchar(20),num int,price double,primary key(id));
+
+-- data insert
+insert into goods values (1,'bike',100,100),(2,'motor',10,1000);
+select * from goods;
+notee
