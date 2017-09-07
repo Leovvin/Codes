19fd437ad7f06ca7df37480dc804c6b86fc235ac
hel39 <hel39@sany.com.cn>
2017-09-07 14:41:15 +0800

    add account manage app

--- SpringDemo/AccountManage/mysql/install.sql
+++ SpringDemo/AccountManage/mysql/install.sql
@@ -1,13 +1,25 @@
 -- table create
-create database if not exists AccountManage default character set utf8;
+drop database if exists AccountManage;
+create database AccountManage default character set utf8;
 use AccountManage;
-drop table if exists account;
-create table account(id bigint,name varchar(20),password varchar(20),primary key(id));
-drop table if exists role;
-create table role(id bigint,name varchar(20),primary key(id));
-drop table if exists privilege;
-create table privilege(id bigint,privilege int,desc varchar(20),primary key(id));
+drop table if exists User;
+create table User(id bigint,name varchar(20) not null,password varchar(20) not null,primary key(id));
+drop table if exists Role;
+create table Role(id bigint,name varchar(20) not null,des varchar(100),primary key(id));
+drop table if exists Privilege;
+create table Privilege(id bigint,type int not null,des varchar(100) not null,primary key(id));
+drop table if exists UserRole;
+create table UserRole(id bigint,userId bigint not null,roleId bigint not null,primary key(id));
+drop table if exists RolePrivilege;
+create table RolePrivilege(id bigint,roleId bigint not null,privilegeId bigint not null,primary key(id));
+drop table if exists Page;
+create table Page(id bigint,name varchar(20),url varchar(20),primary key(id));
+drop table if exists PagePrivilege;
+create table PagePrivilege(id bigint,pageId bigint not null,privilegeId bigint not null,primary key(id));
+
+
 -- data insert
-insert into goods values (1,'bike',100,100),(2,'motor',10,1000);
-select * from goods;
+insert into User values (1,'admin','admin');
+insert into Role values (1,'admin','administrator');
+show tables;
 notee
--- SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/entity/User.java
+++ SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/entity/User.java
@@ -1,4 +1,7 @@
 package com.levon.AccountManage.entity;
 
+import javax.persistence.Entity;
+
+@Entity
 public class User {
 }
