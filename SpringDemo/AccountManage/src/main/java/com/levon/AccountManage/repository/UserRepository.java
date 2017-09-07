987cd11b4d9b975eb1ebf5325a93590f566358f5
hel39 <hel39@sany.com.cn>
2017-09-07 16:28:59 +0800

    add account manage app

--- SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/repository/UserRepository.java
+++ SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.levon.AccountManage.repository;
 
-public class UserRepository {
+import com.levon.AccountManage.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Long> {
 }
