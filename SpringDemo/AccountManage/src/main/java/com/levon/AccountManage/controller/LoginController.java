e199b00973a7aeda84590e9a7de0d7c59417aa5a
levon <levon.he@outlook.com>
2017-09-21 07:03:08 +0800

    accountmanage app add ui part

--- SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/controller/LoginController.java
+++ SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/controller/LoginController.java
@@ -1,14 +1,9 @@
 package com.levon.AccountManage.controller;
 
-import com.levon.AccountManage.bo.Login;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Map;
 
 @Controller
 public class LoginController {
