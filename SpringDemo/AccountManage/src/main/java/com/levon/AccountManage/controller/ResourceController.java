d2366481ab4927338e4cecad581914da14c05da2
hel39 <hel39@sany.com.cn>
2017-09-23 16:07:31 +0800

    AccountManage data refactor

--- SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/controller/ResourceController.java
+++ SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/controller/ResourceController.java
@@ -3,6 +3,7 @@ package com.levon.AccountManage.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -16,5 +17,9 @@ public class ResourceController {
         model.put("id", UUID.randomUUID().toString());
         model.put("content", "Hello World");
         return model;
+    }
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
     }
 }
