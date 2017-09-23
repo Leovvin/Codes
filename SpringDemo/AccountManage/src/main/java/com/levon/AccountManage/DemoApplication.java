65949df0b45086c5e2ec69372c933f261ce118c5
hel39 <hel39@sany.com.cn>
2017-09-23 16:57:27 +0800

    AccountManage data refactor

--- SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/DemoApplication.java
+++ SpringDemo/AccountManage/src/main/java/com/levon/AccountManage/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -20,12 +21,13 @@ public class DemoApplication {
 	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http
-				.httpBasic()
-				.and()
-				.authorizeRequests()
-				.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-				.anyRequest().authenticated();
+            http
+                .httpBasic().and()
+                .authorizeRequests()
+                .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
+                .authenticated().and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 		}
 	}
 }
