92c6b037799c4ea7be1e80c157201e31e5c8e37d
leovvin <leovvin.he@outlook.com>
2020-12-10 14:57:42 +0800

    update

---
+++ SpringDemo/reactDemo/src/main/java/com/example/react/CommandRunner.java
@@ -0,0 +1,17 @@
+package com.example.react;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CommandRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        ConnectionFactory connectionFactory = null;
+        DatabaseClient client = DatabaseClient.create(connectionFactory);
+        client.sql().then()
+    }
+}
