a7d6c7c48f69b38d861bcf9619338cd450ff06d9
leovvin <leovvin.he@outlook.com>
2020-12-10 16:38:41 +0800

    update

---
+++ SpringDemo/shadingDemo/src/main/java/com/example/shading/ShadingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadingDemoApplication.class, args);
+	}
+
+}
